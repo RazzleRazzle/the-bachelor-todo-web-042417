d426d391577d175e2d6b71e949c186bc6f6772c6
Jake Musich <makejusich@gmail.com>
2017-04-20 05:15:28 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,71 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  winner = nil
+  data.each do |key, value|
+    if key == season
+      value.each do |attribute, att_info|
+        if attribute["status"] == "Winner"
+          winner = attribute["name"]
+        end
+      end
+    end
+  end
+  winner.split.first
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  occupant = nil
+  data.each do |key, value|
+    value.each do |attribute, att_info|
+      if attribute["occupation"] == occupation
+        occupant = attribute["name"]
+      end
+    end
+  end
+  occupant
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  counter = 0
+  data.each do |key, value|
+    value.each do |attribute, att_info|
+      if attribute["hometown"] == hometown
+        counter += 1
+      end
+    end
+  end
+  counter
 end
 
 def get_occupation(data, hometown)
-  # code here
+  occupant = nil
+  data.each do |key, value|
+    value.find do |attribute, att_info|
+      if attribute["hometown"] == hometown
+        occupant = attribute["occupation"]
+      end
+    end
+  end
+   occupant
 end
 
+
+
 def get_average_age_for_season(data, season)
-  # code here
+  ages = []
+
+  data.each do |key, value|
+    if key == season
+      value.each do |attribute, att_info|
+        ages << attribute["age"]
+      end
+    end
+  end
+
+  int_ages = ages.collect(&:to_i)
+
+  average = int_ages.inject{ |sum, el| sum + el }.to_f / int_ages.size
+
+  average.round
 end
